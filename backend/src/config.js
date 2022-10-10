08947dff8811c9f7259fb52bc51d34b44dc90e43
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-10-10 22:53:09 +0300

    Predeploy firebase error

--- backend/src/config.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-export default {
-  LOCAL_URI: 'mongodb://127.0.0.1:27017/?directConnection=true',
-  REMOTE_URI: process.env.MONGO_URI || this.LOCAL_URI,
-  DBNAME: 'polls',
-};
--- backend/src/models/polls.js
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose';
-import { QuestionSchema, UserSchema } from '../schemas/Polls.js';
-export const questionModel = mongoose.model('Question', QuestionSchema);
-export const userModel = mongoose.model('User', UserSchema);
